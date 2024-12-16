32cc2f30c46a99860b358103de13d30fa2db232a
Bendik <playstationbendik@gmail.com>
2024-12-16 09:37:19 +0100

    la til bilde

---
+++ index.js
@@ -0,0 +1,25 @@
+document.addEventListener('keydown', function (event){
+    if (event.key === "w") {
+        console.log("w trykket")
+    }
+})
+
+document.addEventListener('keydown', function (event){
+    if (event.key === "a") {
+        console.log("a trykket")
+    }
+})
+
+document.addEventListener('keydown', function (event){
+    if (event.key === "s") {
+        console.log("s trykket")
+    }
+})
+
+document.addEventListener('keydown', function (event){
+    if (event.key === "d") {
+        console.log("d trykket")
+    }
+})
+
+
