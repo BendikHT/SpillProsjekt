1da7e481e227e563352aa1ea18c2ac67b067ecee
Bendik <playstationbendik@gmail.com>
2024-12-17 10:24:16 +0100

    går litt skatere

--- index.js
+++ index.js
@@ -13,22 +13,22 @@ let posY = 200
 document.addEventListener('keydown', function (event) {
     if (event.key === "w") { 
         console.log("w trykket");
-        posY -= 10; 
+        posY -= 5; 
         character.style.top = posY + "px";
     }
     if (event.key === "a") {
         console.log("a trykket");
-        posX -= 10; 
+        posX -= 5; 
         character.style.left = posX + "px";
     }
     if (event.key === "s") {
         console.log("s trykket");
-        posY += 10; 
+        posY += 5; 
         character.style.top = posY + "px";
     }
     if (event.key === "d") {
         console.log("d trykket");
-        posX += 10;
+        posX += 5;
         character.style.left = posX + "px";
     }
 });
